930f9b2fe1a7879f39ecb6a02733b0e2aae10152
RajaNouman2000 <rajanouman2000@gmail.com>
2023-03-14 22:46:25 +0500

    Created form

--- form_app/views.py
+++ form_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
+from .forms import ApplicationForm
 
 # Create your views here.
+
+
+class Home(View):
+    def get(self, request):
+        form = ApplicationForm()
+        return render(request, 'form.html', {'form': form})
