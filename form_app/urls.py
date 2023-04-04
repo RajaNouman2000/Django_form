d1360b37c0ab76a19e5ed6bc85a7405e76ffa3c3
RajaNouman2000 <rajanouman2000@gmail.com>
2023-04-04 18:19:17 +0500

    js

--- form_app/urls.py
+++ form_app/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from . views import Home
+from . import views
 
 urlpatterns = [
 
-    path("", Home.as_view(), name="home"),
+    path("", views.form_view, name="home"),
 ]
--- form_app/views.py
+++ form_app/views.py
@@ -1,13 +1,30 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.views import View
-from .models import Registeration_form
+from . models import Registeration_form
 from .form import ApplicationForm
+
 
 # Create your views here.
 
 
-class Home(View):
-    def get(self, request):
-        form = ApplicationForm()
-        return render(request, 'form.html', {'form': form})
+def form_view(request):
+    form = ApplicationForm()
+
+    if request.method == 'POST':
+        form = ApplicationForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            print(cd)
+            uc = Registeration_form(
+                # first_name,last_name,comment
+                name=cd['name'],
+                address=cd['address'],
+                field=cd['field'],
+            )
+            uc.save()
+            return JsonResponse({
+                'message': 'success'
+            })
+
+    return render(request, 'form.html', {'form': form})
