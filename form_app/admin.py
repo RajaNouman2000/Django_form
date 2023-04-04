7d01df0c606cb43d1ddba140d7684d13654cb451
RajaNouman2000 <rajanouman2000@gmail.com>
2023-04-04 13:56:28 +0500

    js

--- form_app/admin.py
+++ form_app/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
-from .models import Registeration_form
+from .models import Registeration_form, Person
 # Register your models here.
+
+
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ("last_name", "first_name")
+
 
 admin.site.register(Registeration_form)
--- form_app/models.py
+++ form_app/models.py
@@ -3,6 +3,14 @@ from django.db import models
 # Create your models here.
 
 
+class Person(models.Model):
+    last_name = models.CharField(max_length=20)
+    first_name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"{self.last_name}, {self.first_name}"
+
+
 class Registeration_form(models.Model):
     name = models.CharField(max_length=50)
     address = models.CharField(max_length=100)
